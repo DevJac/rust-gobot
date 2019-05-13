9e57eeab67247d54febacd337ebe93f8162762b9
Devin Jacobs <dbuttonz@gmail.com>
2019-05-13 11:30:57 -0600

    Point.neighbors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-use BoardPosition::{Empty, Black, White};
+#![warn(
+    warnings,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rustdoc,
+    unused
+)]
+#![allow(dead_code, non_snake_case)] // TODO: Remove
+
+use BoardPosition::{Black, Empty, White};
+use Direction::{Down, Left, Right, Up};
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 #[derive(Clone, Copy, Debug)]
 enum BoardPosition {
@@ -19,12 +38,69 @@ impl BoardPosition {
 
 #[derive(Clone, Copy, Debug)]
 struct Point {
-    x: u8,
-    y: u8,
+    x: i8,
+    y: i8,
+}
+
+fn P(x: i8, y: i8) -> Point {
+    Point { x, y }
 }
 
 impl Point {
-    // TODO: neighbors
+    fn neighbors(self) -> NeighborsIter {
+        NeighborsIter::new(self)
+    }
+}
+
+impl std::ops::Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+struct NeighborsIter {
+    point: Point,
+    neighbor: Option<Direction>,
+}
+
+impl NeighborsIter {
+    fn new(point: Point) -> Self {
+        Self {
+            point,
+            neighbor: Some(Up),
+        }
+    }
+}
+
+impl Iterator for NeighborsIter {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Point> {
+        match self.neighbor {
+            Some(Up) => {
+                self.neighbor = Some(Right);
+                Some(self.point + P(0, 1))
+            }
+            Some(Right) => {
+                self.neighbor = Some(Down);
+                Some(self.point + P(1, 0))
+            }
+            Some(Down) => {
+                self.neighbor = Some(Left);
+                Some(self.point + P(0, -1))
+            }
+            Some(Left) => {
+                self.neighbor = None;
+                Some(self.point + P(-1, 0))
+            }
+            None => None,
+        }
+    }
 }
 
 #[derive(Debug)]
