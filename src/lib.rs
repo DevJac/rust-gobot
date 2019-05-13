29f0f5816b95dfd7a5146421e15b5bf48295ca36
Devin Jacobs <dbuttonz@gmail.com>
2019-05-13 11:06:41 -0600

    Basic structs and enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use BoardPosition::{Empty, Black, White};
+
+#[derive(Clone, Copy, Debug)]
+enum BoardPosition {
+    Empty,
+    Black,
+    White,
+}
+
+impl BoardPosition {
+    fn other(self) -> Self {
+        match self {
+            Empty => Empty,
+            Black => White,
+            White => Black,
+        }
     }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+impl Point {
+    // TODO: neighbors
+}
+
+#[derive(Debug)]
+struct Board {
+    size: u8,
+    board: Vec<BoardPosition>,
+    liberties: Vec<BoardPosition>,
+    history: Vec<Board>,
+}
+
+impl Board {
+    // TODO
 }
